e80496916b00f7643754a2c34b641605b408eb77
lior.px@gmail.com <Lior Rabinovich>
2019-04-13 22:25:05 +0300

    Added redux and react redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">{this.props.siteName}</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link active" href="#">Home</a>
+              <a className="nav-item nav-link" href="#">Users</a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    siteName: state.siteName
+  }
+}
+
+const mapDispatchToProps = disaptch => {
+  return {
+    changeSiteName(newSiteName) {
+      disaptch({
+        type: "CHANGE_SITE_NAME",
+        payload: newSiteName
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const initState = {
+    siteName: 'Youtube React Redux'
+}
+
+const reducer = (state = initState, action) => {
+
+    switch(action.type){
+        case "CHANGE_SITE_NAME":
+            state = {...state, siteName: action.payload}
+        break;
+    }
+
+    return state;
+}
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
