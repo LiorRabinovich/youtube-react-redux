090433cf19c10abb6702134d61a1611d3585240f
lior.px@gmail.com <Lior Rabinovich>
2019-04-19 15:49:30 +0300

    Lifecycle React Component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
-    constructor() {
+    constructor(props) {
         super();
 
         this.state = {
@@ -10,8 +10,10 @@ class LoginForm extends Component {
                 password: ''
             }
         }
+
+        console.log('constructor', props);
     }
-    handlerChangeUser = (property,value) => {
+    handlerChangeUser = (property, value) => {
         let user = this.state.user;
         user[property] = value;
 
@@ -19,9 +21,18 @@ class LoginForm extends Component {
             user
         })
     }
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
     render() {
+        console.log('render');
         return (
             <form className="login-form">
+                <div className="alert alert-primary">
+                    {this.props.msg}
+                </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
                     <input type="email" value={this.state.user.email} onChange={(e) => this.handlerChangeUser('email', e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
@@ -34,6 +45,33 @@ class LoginForm extends Component {
             </form>
         )
     }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('componentWillReceiveProps', nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate', nextProps, nextState);
+
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate', nextProps, nextState);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', prevProps, prevState);
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
 }
+
 
 export default LoginForm;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import LoginForm from '../components/LoginForm';
 
 class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            msg: 'Hello World',
+            isEnableLoginForm: true
+        }
+
+        setTimeout(() => {
+            this.setState({
+                msg: 'Hello World Changed!'
+            })
+        }, 2000)
+    }
+
+    toggleLoginForm = () => {
+        this.setState({
+            isEnableLoginForm: !this.state.isEnableLoginForm
+        });
+    }
+
     render() {
+        let loginForm = '';
+        if (this.state.isEnableLoginForm) {
+            loginForm = <LoginForm msg={this.state.msg} />;
+        }
+
         return (
             <div className="login-page">
-                <LoginForm />
+                {loginForm}
+                <button onClick={this.toggleLoginForm}>Toggle Login Form</button>
             </div>
         )
     }
